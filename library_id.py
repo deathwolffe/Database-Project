592505036de8d430e9603779f0c88098988fa960
deathwolffe <froatskaylee@gmail.com>
2026-08-09 22:07:01 -0400

    third milestone for project
    fully functional script to manage PostgreSQL database "library_froa0019"

--- library_id.py
+++ library_id.py
@@ -8,7 +8,7 @@
 #define imports
 import sys
 import psycopg2
-
+from datetime import date
 
 #define constants
 answer = 42
@@ -42,35 +42,70 @@ def get_menu_option():
 def get_attribute_value():
     return input("please input a field value:").lower()
 
-# Purpose:   skeleton function, to be completed later
-# Parameter:
-# Return:
+# Purpose:   Query all books with all attributes, including author and publisher, sorted by title
+# Parameter: cursor
+# Return: none
 def query_book_attributes():
-    print("Book records")
+    print("Book records:")
+    cursor.execute("SELECT * FROM book")
+    display_query()
 
-# Purpose:   skeleton function, to be completed later
-# Parameter:
-# Return:
+# Purpose:   Query all current loans made by a given member and sorted by date
+# Parameter: cursor, member ID
+# Return: none
 def query_loans_by_member():
-    print("Member loan records")
+    print("Member loan records:")
+    member = query_member_id()
+    cursor.execute(f"SELECT * FROM loan WHERE member_id = {member} ORDER BY loan_date")
+    display_query()
 
-# Purpose:   skeleton function, to be completed later
-# Parameter:
-# Return:
+# Purpose:   Query all loans that are overdue
+# Parameter: cursor
+# Return: none
 def query_overdue_loans():
-    print("Overdue loans")
+    print("Overdue loans:")
+    cursor.execute("""
+SELECT * FROM loan
+JOIN book_copy ON loan.copy_id = book_copy.copy_id
+JOIN book ON book_copy.isbn = book.isbn
+WHERE (loan_date + rental_days) < CURRENT_DATE
+AND return_date IS NULL
+""")
+    display_query()
 
-# Purpose:   skeleton function, to be completed later
-# Parameter:
-# Return:
+# Purpose:   Query member id based on member phone number
+# Parameter: cursor, member phone number
+# Return: member ID
+def query_member_id():
+    phone = input("please input a member's phone number:")
+    cursor.execute(f"SELECT member_id FROM member WHERE phone = '{phone}'")
+    query = cursor.fetchone()
+    return query[0][0]
+
+# Purpose:   Display query result
+# Parameter: cursor
+# Return: none
+def display_query():
+    query = cursor.fetchall()
+    for row in query:
+        print(row)
+
+# Purpose:   Insert a loan
+# Parameter: cursor, member ID, book copy ID
+# Return: none
 def insert_loan():
-    print("Insert record")
+    member = query_member_id()
+    copy = input("Please input a copy to be loaned:")
+    date = date.today()
+    cursor.execute(f"""INSERT INTO loan (copy_id, member_id, loan_date) VALUES
+({copy}, {member}, '{date}')""")
 
-# Purpose:   skeleton function, to be completed later
-# Parameter:
-# Return:
+# Purpose:   Delete a loan
+# Parameter: cursor, loan ID
+# Return: none
 def delete_loan():
-    print("Delete record")
+    loan = input("Please input a loan to be deleted:")
+    cursor.execute("DELETE FROM loan WHERE loan_id = '{loan}'")
 
 
 ################
